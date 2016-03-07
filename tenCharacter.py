f43eb9e0f0fcb1886ec0abdf8719ffe0ee337f91
Cole Miller <cmiller2019@pingry.org>
2016-03-07 01:24:21 -0500

    Second commit. Fully functional for assignment. Prints limited 10 character version of input to output file while maintaining original input on input file. Also added comments. Works. Submission worthy.

--- tenCharacter.py
+++ tenCharacter.py
@@ -1,18 +1,20 @@
 #Author: Cole Miller
 #Date: 2 March 2016
 
+#Open the two files
 f = open("input.txt", 'r+')
 
 f2 = open("output.txt", 'r+')
 
 read = f.read()
 
-read2 = f2.readlines()
+read2 = f2.read()
 
 text = raw_input("What text would you like to write?\n")
 
+#Write the original input with no limitations to input file 
 f.write(text)
 
+#If the text is more than ten characters write the limited to ten characters version on the ouput file
 if len(text) > 10:
-	f2.write(text)
-
+	f2.write(text[0:10])
